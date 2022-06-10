aade3a18d82d034ce9e86f68347fe2cac941058b
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-10 16:16:18 +0500

    setup bottom navigation

--- app/src/main/java/xyz/teamgravity/containertransformtransitionfab/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/containertransformtransitionfab/presentation/activity/Main.kt
@@ -2,6 +2,9 @@ package xyz.teamgravity.containertransformtransitionfab.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import xyz.teamgravity.containertransformtransitionfab.R
 import xyz.teamgravity.containertransformtransitionfab.databinding.ActivityMainBinding
 
 class Main : AppCompatActivity() {
@@ -12,5 +15,12 @@ class Main : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navigation()
+    }
+
+    private fun navigation() {
+        val controller = (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController
+        binding.bottomNavigation.setupWithNavController(controller)
     }
 }
