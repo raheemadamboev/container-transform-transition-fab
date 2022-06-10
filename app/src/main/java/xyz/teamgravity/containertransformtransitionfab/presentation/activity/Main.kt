64c27e2ee63f2c78ad69f8f3d54cf818f0b70e77
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-10 16:07:16 +0500

    setup main

--- app/src/main/java/xyz/teamgravity/containertransformtransitionfab/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/containertransformtransitionfab/presentation/activity/Main.kt
@@ -1,12 +1,16 @@
 package xyz.teamgravity.containertransformtransitionfab.presentation.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import xyz.teamgravity.containertransformtransitionfab.R
+import androidx.appcompat.app.AppCompatActivity
+import xyz.teamgravity.containertransformtransitionfab.databinding.ActivityMainBinding
 
 class Main : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
