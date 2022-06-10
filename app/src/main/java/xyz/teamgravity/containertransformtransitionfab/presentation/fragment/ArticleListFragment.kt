777601de42dd04306be3f915dac51e7dccf87014
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-10 15:09:08 +0500

    add fragments

---
+++ app/src/main/java/xyz/teamgravity/containertransformtransitionfab/presentation/fragment/ArticleListFragment.kt
@@ -0,0 +1,24 @@
+package xyz.teamgravity.containertransformtransitionfab.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import xyz.teamgravity.containertransformtransitionfab.databinding.FragmentArticleListBinding
+
+class ArticleListFragment : Fragment() {
+
+    private var _binding: FragmentArticleListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentArticleListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/xyz/teamgravity/containertransformtransitionfab/presentation/fragment/FavoriteListFragment.kt
@@ -0,0 +1,24 @@
+package xyz.teamgravity.containertransformtransitionfab.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import xyz.teamgravity.containertransformtransitionfab.databinding.FragmentFavoriteListBinding
+
+class FavoriteListFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentFavoriteListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
