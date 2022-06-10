d752819e055223812991f8542a417a89f6b70ec1
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-10 16:24:05 +0500

    add and setup fab

--- app/src/main/java/xyz/teamgravity/containertransformtransitionfab/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/containertransformtransitionfab/presentation/activity/Main.kt
@@ -2,14 +2,19 @@ package xyz.teamgravity.containertransformtransitionfab.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import xyz.teamgravity.containertransformtransitionfab.NavigationDirections
 import xyz.teamgravity.containertransformtransitionfab.R
+import xyz.teamgravity.containertransformtransitionfab.core.extension.navigateSafely
 import xyz.teamgravity.containertransformtransitionfab.databinding.ActivityMainBinding
 
 class Main : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var controller: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +22,21 @@ class Main : AppCompatActivity() {
         setContentView(binding.root)
 
         navigation()
+        button()
     }
 
     private fun navigation() {
-        val controller = (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController
+        controller = (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController
         binding.bottomNavigation.setupWithNavController(controller)
+    }
+
+    private fun button() {
+        onAdd()
+    }
+
+    private fun onAdd() {
+        binding.addB.setOnClickListener {
+            controller.navigateSafely(NavigationDirections.actionGlobalArticleAdd())
+        }
     }
 }
